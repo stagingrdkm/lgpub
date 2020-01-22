7c99be0f7b9951699ecafd7988b4df7d62ee2bc5
Peter Trekels <peter.trekels@androme.com>
2020-01-22 16:56:55 +0100

    Add date check in download script
    * rpi does not sync date and this introduces half download

--- dac/client/scripts/download.sh
+++ dac/client/scripts/download.sh
@@ -3,6 +3,17 @@
 # flush for simple support on EOS boxes
 iptables -F
 
+# check if date is correctly set
+curl -s https://www.google.com >/dev/null
+if [ $? -eq 60 ]; then
+    echo "Is current date $(date)?"
+    echo "If not, please run:"
+    echo "killall -9 ntpd"
+    echo "ntpd -n -d -q -g time.nrc.ca"
+    exit 0
+fi
+
+
 # config data
 PACKAGE=appcontainerstagingrdk/demo
 
