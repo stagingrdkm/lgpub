007c73a85174de443a00a27107ef074939c6d1f7
Peter Trekels <peter.trekels@androme.com>
2020-01-23 10:01:37 +0100

    Fix issue with sed in gen_config.sh
    * split up sed commands in gen_config.sh
    * add config.json in .gitignore

--- dac/client/scripts/gen_config.sh
+++ dac/client/scripts/gen_config.sh
@@ -6,12 +6,12 @@ cp config.json.template config.json
 
 echo "Platform: $PLATFORM"
 
-EXTRA_ENV=$(sed '/^[[:space:]]*$/d s/\([^ ]*\)[[:space:]]*\([^ ]*\)/  "\1", /' platform/$PLATFORM/env.txt)
+EXTRA_ENV=$(sed '/^[[:space:]]*$/d' platform/$PLATFORM/env.txt | sed 's/\([^ ]*\)[[:space:]]*\([^ ]*\)/  "\1", /')
 TEMP=$(sed 's/#EXTRA_ENV#/%s/' config.json)
 printf "$TEMP" "$EXTRA_ENV" > config.json
 echo "generate extra env: $(echo "$EXTRA_ENV" | wc -l)"
 
-EXTRA_LIBS=$(sed '/^[[:space:]]*$/d s/\([^ ]*\)[[:space:]]*\([^ ]*\)/  { "destination": "\1", "source": "\2", "type": "bind", "options": [ "rbind", "nosuid", "nodev", "ro" ] }, /' platform/$PLATFORM/libs.txt)
+EXTRA_LIBS=$(sed '/^[[:space:]]*$/d' platform/$PLATFORM/libs.txt | sed 's/\([^ ]*\)[[:space:]]*\([^ ]*\)/  { "destination": "\1", "source": "\2", "type": "bind", "options": [ "rbind", "nosuid", "nodev", "ro" ] }, /')
 TEMP=$(sed 's/#EXTRA_LIBS#/%s/' config.json)
 printf "$TEMP" "$EXTRA_LIBS" > config.json
 echo "generate extra libs: $(echo "$EXTRA_LIBS" | wc -l)"
@@ -23,12 +23,12 @@ TEMP=$(sed 's/#EXTRA_GENLIBS#/%s/' config.json)
 printf "$TEMP" "$EXTRA_GENLIBS" > config.json
 echo "generate extra gen libs: $(echo "$EXTRA_GENLIBS" | wc -l)"
 
-EXTRA_FILES=$(sed '/^[[:space:]]*$/d s/\([^ ]*\)[[:space:]]*\([^ ]*\)/  { "destination": "\1", "source": "\2", "type": "bind", "options": [ "rbind", "nosuid", "nodev", "rw" ] }, /' platform/$PLATFORM/files.txt)
+EXTRA_FILES=$(sed '/^[[:space:]]*$/d' platform/$PLATFORM/files.txt | sed 's/\([^ ]*\)[[:space:]]*\([^ ]*\)/  { "destination": "\1", "source": "\2", "type": "bind", "options": [ "rbind", "nosuid", "nodev", "rw" ] }, /')
 TEMP=$(sed 's/#EXTRA_FILES#/%s/' config.json)
 printf "$TEMP" "$EXTRA_FILES" > config.json
 echo "generate extra files: $(echo "$EXTRA_FILES" | wc -l)"
 
-EXTRA_DEVS=$(sed '/^[[:space:]]*$/d s/\([^ ]*\)[[:space:]]*\([^ ]*\)/  { "destination": "\1", "source": "\2", "type": "bind", "options": [ "rbind", "rw" ] }, /' platform/$PLATFORM/devs.txt)
+EXTRA_DEVS=$(sed '/^[[:space:]]*$/d' platform/$PLATFORM/devs.txt | sed 's/\([^ ]*\)[[:space:]]*\([^ ]*\)/  { "destination": "\1", "source": "\2", "type": "bind", "options": [ "rbind", "rw" ] }, /')
 TEMP=$(sed 's/#EXTRA_DEVS#/%s/' config.json)
 printf "$TEMP" "$EXTRA_DEVS" > config.json
 echo "generate extra devs: $(echo "$EXTRA_DEVS" | wc -l)"
