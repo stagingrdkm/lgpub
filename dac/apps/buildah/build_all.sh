3c7999430d39beea2e98b9935a2d0f8e1ccc45cf
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-06-29 14:28:05 +0200

    add gtk-egl-test

--- dac/apps/buildah/build_all.sh
+++ dac/apps/buildah/build_all.sh
@@ -4,6 +4,8 @@ buildah unshare ./build.sh wayland-egl-test latest
 
 buildah unshare ./build.sh qt-egl-test latest
 
+buildah unshare ./build.sh gtk-egl-test latest
+
 buildah unshare ./build.sh flutter
  
 buildah unshare ./build.sh you.i 
