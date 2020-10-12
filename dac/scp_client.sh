f29447cf7e1892a59d195e2b9a0226a7618911d0
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-10-12 13:15:40 +0200

    Make the script working from any directory
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- dac/scp_client.sh
+++ dac/scp_client.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z $1 ]; then
     echo "Usage:"
@@ -7,7 +7,9 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
 EXTRA_SCP_ARGS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-scp $EXTRA_SCP_ARGS ./bin/armv7hl/bin/* root@$1:/usr/bin
-scp $EXTRA_SCP_ARGS ./bin/armv7hl/lib/* root@$1:/usr/lib
-scp $EXTRA_SCP_ARGS -r ./client root@$1:/home/root
+scp $EXTRA_SCP_ARGS ${SCRIPT_DIR}/bin/armv7hl/bin/* root@$1:/usr/bin
+scp $EXTRA_SCP_ARGS ${SCRIPT_DIR}/bin/armv7hl/lib/* root@$1:/usr/lib
+scp $EXTRA_SCP_ARGS -r ${SCRIPT_DIR}/client root@$1:/home/root
