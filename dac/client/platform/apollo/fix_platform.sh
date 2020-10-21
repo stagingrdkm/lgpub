46d9d85d3786edd8c88de6390692130565afd751
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-10-21 14:05:25 +0200

    Add apollo platform (eos based config)
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

---
+++ dac/client/platform/apollo/fix_platform.sh
@@ -0,0 +1,17 @@
+echo fix_platform.sh - skipped
+exit 0
+
+chmod 777 /var
+chmod 777 /var/run
+
+chmod 777 /var/run/weston
+chmod 777 /var/run/weston/wayland-0
+
+chmod 777 /var/run/nxserver
+chmod 777 /var/run/nxserver/nxserver_ipc
+
+chmod 777 /var/run/dbus
+chmod 777 /var/run/dbus/system_bus_socket
+
+chmod 777 /dev
+chmod 777 /dev/nexus
